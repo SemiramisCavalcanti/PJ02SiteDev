792781bd71ddef69bba136b1de022befc02e75d3
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-01-18 17:05:06 -0300

    parteCartaoVirtual

--- paginas/fenixEcho.php
+++ paginas/fenixEcho.php
@@ -43,38 +43,75 @@
     <!-- Linha Serviços -->
 
     <!-- Menu Serviços (Bootstrap) -->
-    <div class="row border border-light rounded p-3">
+    <div class="row">
+        <div class="col-6">
+            <div class="row border border-light rounded p-3">
+                <div class="col-6">
+                    <h5 class="text-light">WebSites</h5>
 
-        <div class="col-3">
-            <h5 class="text-light">Desenvolvimento Web</h5>
+                    <div class="list-group w-75" id="list-tab" role="tablist">
+                        <a class="list-group-item list-group-item-action active list-group-item-danger letramaisescura mt-3" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Institucional</a>
+                        <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Página de Vendas</a>
+                        <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Blog Pessoal</a>
+                        <a class="list-group-item list-group-item-action  list-group-item-danger letramaisescura" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Loja Virtual</a>
+                    </div>
+                </div>
+                <div class="col-6 mt-3">
+                    <div class="tab-content mt-5 text-light text-center" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+                            Site com intuito de apresentar a Empresa, a Equipe, o Portifólio, os Serviços, os Parceiros. <br>
+                            <Strong class="letramaisclara text-center"> "É a sua casa na Internet"</Strong>
+                        </div>
+                        <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+                            Site de página única com o intuito de promover. Seja um Evento, Lançamento, Vendas Rápidas de Serviços
+                            ou Prodtos.</div>
+                        <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">Pessoal ou Profissional,
+                            o blog é um site com o intuito de se comunicar com o mundo. Seja para escrever sobre suas opniões, sua jornada,
+                            compartilhar conhecimento e tantos outros.
 
-            <div class="list-group w-75" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active list-group-item-danger letramaisescura" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Institucional</a>
-                <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Página de Vendas</a>
-                <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Blog Pessoal</a>
-                <a class="list-group-item list-group-item-action  list-group-item-danger letramaisescura" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Loja Virtual</a>
+                        </div>
+                        <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">Site com intuito de vender.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3    ">
+                <div class="col-5 border border-light rounded p-3 text-light"> EMAIL pROFISSIONAL</div>
+                <div class="col-1"></div>
+                <div class="col-6 border border-light rounded p-3 text-light">Criação de Logomarcas</div>
             </div>
         </div>
+        <div class="col-1"></div>
         <div class="col-4">
-            <div class="tab-content mt-5 text-light text-center" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
-                    Site com intuito de apresentar a Empresa, a Equipe, o Portifólio, os Serviços, os Parceiros. <br>
-                    <Strong class="letramaisclara text-center"> "É a sua casa na Internet"</Strong>
-                </div>
-                <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
-                    Site de página única com o intuito de promover. Seja um Evento, Lançamento, Vendas Rápidas de Serviços
-                    ou Prodtos.</div>
-                <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">Pessoal ou Profissional,
-                    o blog é um site com o intuito de se comunicar com o mundo. Seja para escrever sobre suas opniões, sua jornada,
-                    compartilhar conhecimento e tantos outros.
+            <div class="row border border-light rounded p-3 text-light">
+                <h5 class="mb-4">Cartão Virtual</h5>
+                <p class="text-center">Fundamental para o desenvolvimento do negócio o Cartão Virtual é a ferramenta que mantém contato com
+                    o cliente. <br> <Strong class="letramaisclara">É a sua autoapresentação!</Strong> <br> Reforça a <strong class="letramaisclara">credibilidade</strong> e a
+                    <strong class="letramaisclara">legitimidade</strong> do seu negócio.
+                </p>
+                <p class="list-group" id="list-tab" role="tablist "> <strong class="list-group-item list-group-item-action list-group-item-danger letramaisescura">Vantagens do Cartão Virtual</strong> </p>
+                <li class="mt-2 letramaisclara"> Podem ir onde quer que vá!</li>
+                <li class="mt-2 letramaisclara"> Facilidade de Acesso</li>
+                <li class="mt-2 letramaisclara"> Indicação com apenas um click</li>
+                <li class="mt-2 letramaisclara"> Divulgação em cascata</li>
+                <li class="mt-2 letramaisclara"> Nunca acaba</li>
+                <li class="mt-2 letramaisclara"> Facilidade para Atualizações</li>
+                <li class="mt-2 letramaisclara"> Menor custo</li>
+                <p></p>
 
-                </div>
-                <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">Site com intuito de vender.
-                </div>
+
             </div>
         </div>
-        <div class="col-4"></div>
-        <div class="col-4"></div>
+
+
+
+
+
+
+
+
+
+
     </div>
     <!-- Fim Linha Serviços  -->
 
