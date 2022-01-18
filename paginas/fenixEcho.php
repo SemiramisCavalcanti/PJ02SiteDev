34d7abdcb251a9b6b7a5db4447f6c49eab80c05b
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-01-18 16:17:06 -0300

    parte serviços site

--- paginas/fenixEcho.php
+++ paginas/fenixEcho.php
@@ -12,10 +12,18 @@
             <!-- Coluna Partes/Paginas -->
             <div class="col-6">
                 <div class="row">
-                    <div class="col letrasmenu mt-5"> <h4>Serviços</h4> </div>
-                    <div class="col letrasmenu mt-5"> <h4>Portifólio</h4> </div>
-                    <div class="col letrasmenu mt-5"> <h4>Sobre</h4> </div>
-                    <div class="col letrasmenu mt-5"> <h4>Contato</h4> </div>
+                    <div class="col letrasmenu mt-5">
+                        <h4>Serviços</h4>
+                    </div>
+                    <div class="col letrasmenu mt-5">
+                        <h4>Portifólio</h4>
+                    </div>
+                    <div class="col letrasmenu mt-5">
+                        <h4>Sobre</h4>
+                    </div>
+                    <div class="col letrasmenu mt-5">
+                        <h4>Contato</h4>
+                    </div>
                 </div>
             </div>
             <!-- Fim Coluna Partes/Paginas -->
@@ -33,24 +41,40 @@
 <div class="container">
 
     <!-- Linha Serviços -->
+
     <!-- Menu Serviços (Bootstrap) -->
     <div class="row border border-light rounded p-3">
+
+        <div class="col-3">
+            <h5 class="text-light">Desenvolvimento Web</h5>
+
+            <div class="list-group w-75" id="list-tab" role="tablist">
+                <a class="list-group-item list-group-item-action active list-group-item-danger letramaisescura" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Institucional</a>
+                <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Página de Vendas</a>
+                <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Blog Pessoal</a>
+                <a class="list-group-item list-group-item-action  list-group-item-danger letramaisescura" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Loja Virtual</a>
+            </div>
+        </div>
         <div class="col-4">
-            <div class="list-group" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Home</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Profile</a>
-                <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-                <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Settings</a>
+            <div class="tab-content mt-5 text-light text-center" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+                    Site com intuito de apresentar a Empresa, a Equipe, o Portifólio, os Serviços, os Parceiros. <br>
+                    <Strong class="letramaisclara text-center"> "É a sua casa na Internet"</Strong>
+                </div>
+                <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+                    Site de página única com o intuito de promover. Seja um Evento, Lançamento, Vendas Rápidas de Serviços
+                    ou Prodtos.</div>
+                <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">Pessoal ou Profissional,
+                    o blog é um site com o intuito de se comunicar com o mundo. Seja para escrever sobre suas opniões, sua jornada,
+                    compartilhar conhecimento e tantos outros.
+
+                </div>
+                <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">Site com intuito de vender.
+                </div>
             </div>
         </div>
-        <div class="col-8">
-            <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
-                <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
-                <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-                <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
-            </div>
-        </div>
+        <div class="col-4"></div>
+        <div class="col-4"></div>
     </div>
     <!-- Fim Linha Serviços  -->
 
