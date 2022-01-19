c7bc950e69e53a22053ba29f9b16619f542e1a71
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-01-19 13:08:16 -0300

    parte contato

--- paginas/fenixEcho.php
+++ paginas/fenixEcho.php
@@ -76,7 +76,7 @@
                     </div>
                 </div>
             </div>
-            <div class="row mt-3    ">
+            <div class="row mt-3">
                 <div class="col-6 border border-light rounded p-3 text-light">
                     <h5> E-mail Profissional</h5>
 
@@ -99,7 +99,7 @@
         <div class="col-1"></div>
         <div class="col-4">
             <div class="row border border-light rounded p-3 text-light">
-                <h5 class="mb-4">Cartão Virtual</h5>
+                <h5 class="mb-4">Cartão de Visita Virtual</h5>
                 <p class="text-center">Fundamental para o desenvolvimento do negócio o Cartão Virtual é a ferramenta que mantém contato com
                     o cliente. <br> <Strong class="letramaisclara">É a sua autoapresentação!</Strong> <br> Reforça a <strong class="letramaisclara">credibilidade</strong> e a
                     <strong class="letramaisclara">legitimidade</strong> do seu negócio.
@@ -158,7 +158,43 @@
         </div>
 
         <!-- Coluna Contato -->
-        <div class="col-5"></div>
+        <div class="col-5 text-light">
+            <h4 class="mt-5 ml-3">
+                Contato
+            </h4>
+            <!-- Fomulário de Envio Contato -->
+                <form method="post" action="formulario_php.php" class="mt-5">
+                    <div class="form-group">
+                        <select for='contact_client' class="form-control" id="contact_client">
+                            <option>Todos os serviços</option>
+                            <option>Desenvolvimento de Site</option>
+                            <option>E-mail Profissional</option>
+                            <option>Cartão de Visita Virtual</option>
+                            <option>Logomarca</option>
+                        </select>
+                    </div>
+                    <div class="form-group mt-3">
+                        <input for="fullname" type="text" class="form-control" id="fullname" placeholder="Nome Completo">
+                    </div>
+                    <div class="row mt-3">
+                        <div class="form-group col">
+                            <input for="email" type="email" class="form-control" id="email" placeholder="E-mail">
+                        </div>
+                        <div class="form-group col">
+                            <input for="phone" type="text" class="form-control" id="phone" placeholder="(xx) x.xxxx-xxxx">
+                        </div>
+                    </div>
+                    <div class="form-group mt-3">
+                        <textarea for="communication" class="form-control" id="communication" rows="3" placeholder="Conte-nos um pouco sobre sua empresa e como podemos lhe ajudar!"></textarea>
+                    </div>
+                    <div class="text-right mt-3">
+                        <button class="btn btn-light" type="submit"> Enviar </button>
+                    </div>
+    
+                </form>
+            <!-- Fim Fomulário de Envio Contato -->
+
+        </div>
         <!-- Fim Coluna Contato -->
 
     </div>
