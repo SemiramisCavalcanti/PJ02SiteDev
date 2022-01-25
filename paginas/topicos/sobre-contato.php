edae2fe49ff914691d39728ce1fc72822fac96cf
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-01-24 21:18:19 -0300

    pagina Sobre

--- paginas/topicos/sobre-contato.php
+++ paginas/topicos/sobre-contato.php
@@ -1,83 +1,136 @@
-   <!-- Linha Sobre/Contato-->
+   <!-- Linha Sobre-->
    <div class="row">
-        <!-- Coluna Sobre -->
-        <div class="col-6 text-light mt-5">
-            <h4>
-                A Empresa
-            </h4>
-            <p class="text-center">Atuamos no mercado de tecnologia na construção virtual da sua empresa, priorizando a qualidade para
-                melhor alcançar os objetivos de nossos clientes, oferecemos serviços como: desenvolvimento de sites,
-                e-mail profissional, cartão de visitas virtual e criação de logomarcas.
-            </p>
-            <div class="row">
-                <div class="col">
-                    <img src="http://placehold.it/250x200" alt="">
-                </div>
-                <div class="col">
-                    <h5 class="mt-3">
-                        Quem Sou?
-                    </h5>
-                    <p>Dev HTML | CSS | JS | PHP
-                        <br>
-                        Universitária em Análise e Desenvolvimento de Sistemas
-                    </p>
-                    <p></p>
-                </div>
-            </div>
-        </div>
-        <!-- Fim Coluna Sobre -->
-        <div class="col-1 linhaVertical rounded">
-        </div>
-        <!-- Coluna Contato -->
-        <div class="col-5 text-light">
-            <h4 class="mt-5 ml-3">
-                Contato
-            </h4>
-            <!-- Fomulário de Envio Contato -->
-                <form method="post" action="formulario_php.php" class="mt-5">
-                    <div class="form-group">
-                        <select for='contact_client' class="form-control" id="contact_client">
-                            <option>Todos os serviços</option>
-                            <option>Desenvolvimento de Site</option>
-                            <option>E-mail Profissional</option>
-                            <option>Cartão de Visita Virtual</option>
-                            <option>Logomarca</option>
-                        </select>
-                    </div>
-                    <div class="form-group mt-3">
-                        <input for="fullname" type="text" class="form-control" id="fullname" placeholder="Nome Completo">
-                    </div>
-                    <div class="row mt-3">
-                        <div class="form-group col">
-                            <input for="email" type="email" class="form-control" id="email" placeholder="E-mail">
-                        </div>
-                        <div class="form-group col">
-                            <input for="phone" type="text" class="form-control" id="phone" placeholder="(xx) x.xxxx-xxxx">
-                        </div>
-                    </div>
-                    <div class="form-group mt-3">
-                        <textarea for="communication" class="form-control" id="communication" rows="3" placeholder="Conte-nos um pouco sobre sua empresa e como podemos lhe ajudar!"></textarea>
-                    </div>
-                    <div class="text-right mt-3">
-                        <button class="btn btn-light" type="submit"> Enviar </button>
-                    </div>
-    
-                </form>
-            <!-- Fim Fomulário de Envio Contato -->
-        </div>
-        <!-- Fim Coluna Contato -->
-    </div>
-    <!-- Fim Linha Sobre/Contato -->
-    
-    <!-- Linha Reuniões On-line/Localização -->
-    <div class="row">
-        <!-- Coluna Reuniões On-line -->
-        <div class="col">
-        </div>
-        <!-- Fim Coluna Reuniões On-line -->
-        <!-- Coluna Localização -->
-        <div class="col">
-        </div>
-        <!-- Fim Coluna Localização -->
-    </div>
-    <!-- Fim Linha Reuniões On-line/Localização -->
+       <!-- Coluna 01 -->
+       <div class="col-6 text-light mt-5">
+           <!-- A empresa -->
+           <h4>
+               A Empresa
+           </h4>
+           <p class="text-center">Atuamos no mercado de tecnologia na construção virtual da sua empresa, priorizando a qualidade para
+               melhor alcançar os objetivos de nossos clientes, oferecemos serviços como: desenvolvimento de sites,
+               e-mail profissional, cartão de visitas virtual e criação de logomarcas.
+           </p>
+           <div class="row">
+               <div class="col">
+                   <img src="http://placehold.it/250x200" alt="">
+               </div>
+               <!-- Quem Sou -->
+               <div class="col">
+                   <h5 class="mt-3">
+                       Quem Sou?
+                   </h5>
+                   <p>Dev HTML | CSS | JS | PHP
+                       <br>
+                       Universitária em Análise e Desenvolvimento de Sistemas
+                   </p>
+                   <p></p>
+               </div>
+
+           </div>
+       </div>
+       <!-- Fim Coluna 01 -->
+       <div class="col-1 linhaVertical rounded">
+       </div>
+       <!-- Coluna 02 -->
+       <div class="col-5 text-light mt-5 ">
+           <div class="card m-auto mt-5 ">
+               <div class="card-header  mt-3 titulocard elevation-4">
+
+               </div>
+               <div class="card-body mt-1">
+                   <div class="text-center letramaisescura">
+                       <strong>Ter um ambiente virtual não é mais um diferencial, <br> é um requisito do mercado. </strong>
+                       <br>
+                       Na Era Digital onde nossas referências, produtos/serviços, parceiros, projetos e feedbacks de clientes estão nas pontas dos dedos,
+                       temos a facilidade de ofertar, vender e entregar nossos produtos tudo de forma on-line.
+                       <br>
+                       <strong>Então um ambiente Virtual se tornou essencial e nós ajudamos você, a sua marca e a sua empresa a se conectar com o mundo e suas oportunidades.</strong>
+                       <hr>
+                   </div>
+
+               </div>
+
+               <!-- 
+    texto para colocar
+               <div>
+                       E quais seus ganhos reais?
+                   </div>
+                   <div class="text-center">
+                       <strong> - Primeiro, aumenta seu alcance!</strong> Seus produtos e/ou serviços conhecidos nos mais diversos lugares! <br>
+                       <strong> Ah! E transmite confiança e credibilidade!</strong> - Quantas vezes você procurou os comentários na internet sobre determinado local? <br>
+                       E se comentam é porque vale a pena comprar naquele local. <strong>Mantenha seus clientes satisfeitos!
+                           Isto só Fortalece sua marca.</strong> <br> Também <strong>Cria autoridade,</strong> faz de você respeitado, referência.
+                       E o melhor que tudo isso reflete em <strong>suas vendas, aumentando-as</strong>.
+                       <br>
+                       Venha fazer negócio! Peça já seu orçamento!
+                       <hr>
+                   </div> -->
+
+
+
+
+
+
+
+
+
+
+
+           </div>
+           <!-- Fim Coluna Contato -->
+       </div>
+       <!-- Fim Linha Sobre -->
+       <!-- Linha Contato  -->
+       <div class="col-5 text-light">
+           <h4 class="mt-5 ml-3">
+               Contato
+           </h4>
+           <!-- Fomulário de Envio Contato -->
+           <form method="post" action="formulario_php.php" class="mt-5">
+               <div class="form-group">
+                   <select for='contact_client' class="form-control" id="contact_client">
+                       <option>Todos os serviços</option>
+                       <option>Desenvolvimento de Site</option>
+                       <option>E-mail Profissional</option>
+                       <option>Cartão de Visita Virtual</option>
+                       <option>Logomarca</option>
+                   </select>
+               </div>
+               <div class="form-group mt-3">
+                   <input for="fullname" type="text" class="form-control" id="fullname" placeholder="Nome Completo">
+               </div>
+               <div class="row mt-3">
+                   <div class="form-group col">
+                       <input for="email" type="email" class="form-control" id="email" placeholder="E-mail">
+                   </div>
+                   <div class="form-group col">
+                       <input for="phone" type="text" class="form-control" id="phone" placeholder="(xx) x.xxxx-xxxx">
+                   </div>
+               </div>
+               <div class="form-group mt-3">
+                   <textarea for="communication" class="form-control" id="communication" rows="3" placeholder="Conte-nos um pouco sobre sua empresa e como podemos lhe ajudar!"></textarea>
+               </div>
+               <div class="text-right mt-3">
+                   <button class="btn btn-light" type="submit"> Enviar </button>
+               </div>
+
+           </form>
+           <!-- Fim Fomulário de Envio Contato -->
+       </div>
+
+
+
+
+
+       <!-- Linha Reuniões On-line/Localização -->
+       <div class="row">
+           <!-- Coluna Reuniões On-line -->
+           <div class="col">
+           </div>
+           <!-- Fim Coluna Reuniões On-line -->
+           <!-- Coluna Localização -->
+           <div class="col">
+           </div>
+           <!-- Fim Coluna Localização -->
+       </div>
+       <!-- Fim Linha Reuniões On-line/Localização -->
