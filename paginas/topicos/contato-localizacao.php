786e0e3786c13f74eac49eea4c536d1700f9228a
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-01-25 09:20:13 -0300

    localização

--- paginas/topicos/contato-localizacao.php
+++ paginas/topicos/contato-localizacao.php
@@ -1,54 +1,61 @@
+<div class="row">
+    <div class="col-5 text-light">
+        <h4 class="mt-5 ml-3">
+            Contato
+        </h4>
+        <!-- Fomulário de Envio Contato -->
+        <form method="post" action="formulario_php.php" class="mt-5">
+            <div class="form-group">
+                <select for='contact_client' class="form-control" id="contact_client">
+                    <option>Todos os serviços</option>
+                    <option>Desenvolvimento de Site</option>
+                    <option>E-mail Profissional</option>
+                    <option>Cartão de Visita Virtual</option>
+                    <option>Logomarca</option>
+                </select>
+            </div>
+            <div class="form-group mt-3">
+                <input for="fullname" type="text" class="form-control" id="fullname" placeholder="Nome Completo">
+            </div>
+            <div class="row mt-3">
+                <div class="form-group col">
+                    <input for="email" type="email" class="form-control" id="email" placeholder="E-mail">
+                </div>
+                <div class="form-group col">
+                    <input for="phone" type="text" class="form-control" id="phone" placeholder="(xx) x.xxxx-xxxx">
+                </div>
+            </div>
+            <div class="form-group mt-3">
+                <textarea for="communication" class="form-control" id="communication" rows="3" placeholder="Conte-nos um pouco sobre sua empresa e como podemos lhe ajudar!"></textarea>
+            </div>
+            <div class="text-right mt-3">
+                <button class="btn btn-light" type="submit"> Enviar </button>
+            </div>
+
+        </form>
+        <!-- Fim Fomulário de Envio Contato -->
+    </div>
+    <div class="col-5 p-5">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d987.9636949564411!2d-34.836090670849856!3d-7.910235071299581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab3e41689ca26d%3A0x1f6fa8664d23562a!2sAv.%20Costa%20Azul%2C%201208-1066%20-%20Pau%20Amarelo%2C%20Paulista%20-%20PE%2C%2053433-240!5e0!3m2!1spt-BR!2sbr!4v1643112718567!5m2!1spt-BR!2sbr" width="500" class="border border-5 rounded" height="400" allowfullscreen="" loading="lazy"></iframe>
+
+    </div>
+
+</div>
 <!-- Linha Contato  -->
-<div class="col-5 text-light">
-           <h4 class="mt-5 ml-3">
-               Contato
-           </h4>
-           <!-- Fomulário de Envio Contato -->
-           <form method="post" action="formulario_php.php" class="mt-5">
-               <div class="form-group">
-                   <select for='contact_client' class="form-control" id="contact_client">
-                       <option>Todos os serviços</option>
-                       <option>Desenvolvimento de Site</option>
-                       <option>E-mail Profissional</option>
-                       <option>Cartão de Visita Virtual</option>
-                       <option>Logomarca</option>
-                   </select>
-               </div>
-               <div class="form-group mt-3">
-                   <input for="fullname" type="text" class="form-control" id="fullname" placeholder="Nome Completo">
-               </div>
-               <div class="row mt-3">
-                   <div class="form-group col">
-                       <input for="email" type="email" class="form-control" id="email" placeholder="E-mail">
-                   </div>
-                   <div class="form-group col">
-                       <input for="phone" type="text" class="form-control" id="phone" placeholder="(xx) x.xxxx-xxxx">
-                   </div>
-               </div>
-               <div class="form-group mt-3">
-                   <textarea for="communication" class="form-control" id="communication" rows="3" placeholder="Conte-nos um pouco sobre sua empresa e como podemos lhe ajudar!"></textarea>
-               </div>
-               <div class="text-right mt-3">
-                   <button class="btn btn-light" type="submit"> Enviar </button>
-               </div>
-
-           </form>
-           <!-- Fim Fomulário de Envio Contato -->
-       </div>
 
 
 
 
 
-       <!-- Linha Reuniões On-line/Localização -->
-       <div class="row">
-           <!-- Coluna Reuniões On-line -->
-           <div class="col">
-           </div>
-           <!-- Fim Coluna Reuniões On-line -->
-           <!-- Coluna Localização -->
-           <div class="col">
-           </div>
-           <!-- Fim Coluna Localização -->
-       </div>
-       <!-- Fim Linha Reuniões On-line/Localização -->
+<!-- Linha Reuniões On-line/Localização -->
+<div class="row">
+    <!-- Coluna Reuniões On-line -->
+    <div class="col">
+    </div>
+    <!-- Fim Coluna Reuniões On-line -->
+    <!-- Coluna Localização -->
+    <div class="col">
+    </div>
+    <!-- Fim Coluna Localização -->
+</div>
+<!-- Fim Linha Reuniões On-line/Localização -->
