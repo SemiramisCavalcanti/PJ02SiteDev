fd2557376b764ce1d6d8e40927d0eae90695ee04
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-01-25 10:27:19 -0300

    iclusão script jquery

--- paginas/include/footer.php
+++ paginas/include/footer.php
@@ -19,6 +19,7 @@
     <script src="https://ajax.googleapis.com/ajax/"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="plugins\bootstrap-5.1.3-dist\bootstrap-5.1.3-dist\js\bootstrap.bundle.js"></script>
+    <script src="plugins/js/jquery-3.5.1.min.js"></script>
 
 </body>
 
