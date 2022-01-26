19f98b616b344710b2adbc6bc795d2afbf2e2ba5
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-01-26 16:47:02 -0300

    redes sociais icons

--- paginas/fenixEcho.php
+++ paginas/fenixEcho.php
@@ -1,18 +1,25 @@
 <!-- Menu Principal-->
 <div class="menuprincipal my-2 w-100 rounded">
-    <div class="container">
-        <div class="row menuprincipal mb-5">
+    <div class="container container-fluid">
+        <div class="row menuprincipal mb-5" id="home">
 
             <!-- Coluna Logomarca -->
-            <div class="col-sm logomarca">
+            <div class="col-3 logomarca ">
                 <img  src="img/FenixEchoBranca.png" alt="" class="mt-2 img-fluid">
             </div>
             <!-- Fim Coluna Logomarca -->
 
             <!-- Coluna Partes/Paginas -->
-            <div class="col-md">
+            <div class="col-7">
                 <div class="row">
-                    <div class="col letrasmenu mt-5">
+                    <div class="col-3 letrasmenu mt-5">
+                        <h5>
+                            <a class="text-decoration-none letramaisescura" href="#home">
+                                Home
+                            </a>
+                        </h5>
+                    </div>
+                    <div class="col-3 letrasmenu mt-5">
                         <h5>
                             <a class="text-decoration-none letramaisescura" href="#servicos">
                                 Serviços
@@ -26,15 +33,15 @@
                             </a>
                         </h5>
                     </div> -->
-                    <div class="col letrasmenu mt-5">
+                    <div class="col-3 letrasmenu mt-5">
                         <h5>
-                            <a class="text-decoration-none letramaisescura" href="#sobre">
+                            <a class="text-decoration-none text-light" href="#sobre">
                                 Sobre
                             </a>
                         </h5>
 
                     </div>
-                    <div class="col letrasmenu mt-5">
+                    <div class="col-3 letrasmenu mt-5">
                         <h5>
                             <a class="text-decoration-none letramaisescura" href="#contato">
                                 Contato
@@ -46,19 +53,17 @@
             <!-- Fim Coluna Partes/Paginas -->
 
             <!-- Coluna Rede Sociais -->
-            <div class="col-md">
-
-
-                <a href="" class="link-light"> <i class="fa fa-instagram"></i> </a>
-                <a href=""> <i class="fa fa-github"></i> </a>
-                <a href=""> <i class="fa fa-linkedin"></i> </a>
+            <div class="col-2 redessociais container-fluid">
+                <a href="" class="link-light text-decoration-none"> <i class="fab fa-instagram"></i> </a>
+                <a href="" class="link-light text-decoration-none"> <i class="fab fa-github"></i> </a>
+                <a href="" class="link-light text-decoration-none"> <i class="fab fa-linkedin"></i> </a>
          
 
             </div>
 
             <!-- Fim Coluna Redes Sociais -->
 
-        </div>
+        <div>
     </div>
 </div>
 <!-- Fim Menu Principal -->
