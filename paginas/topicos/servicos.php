5fd19fd9dc4b6d46db86cc035209842bc7d4fc8e
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-05-19 00:20:28 -0300

    atualizações de serviços - nomenclaturas

--- paginas/topicos/servicos.php
+++ paginas/topicos/servicos.php
@@ -13,9 +13,9 @@
 
                         <div class="list-group w-75" id="list-tab" role="tablist">
                             <a class="list-group-item list-group-item-action active list-group-item-danger letramaisescura mt-3" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Institucional</a>
-                            <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Página de Vendas</a>
-                            <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Blog Pessoal</a>
-                            <a class="list-group-item list-group-item-action  list-group-item-danger letramaisescura" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Loja Virtual</a>
+                            <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Landing Page</a>
+                            <a class="list-group-item list-group-item-action list-group-item-danger letramaisescura" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Blog</a>
+                            <a class="list-group-item list-group-item-action  list-group-item-danger letramaisescura" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">E-commerce</a>
                         </div>
                     </div>
                     <div class="col-md mt-3 ">
@@ -29,7 +29,7 @@
                                 ou Prodtos.</div>
                             <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">Pessoal ou Profissional,
                                 o blog é um site com o intuito de se comunicar com o mundo. Seja para escrever sobre suas opniões, sua jornada,
-                                compartilhar conhecimento e tantos outros.
+                                compartilhar conhecimento e tantos outros. Então nada mais justo que um site personalizado!!
 
                             </div>
                             <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">Site com intuito de vender.
@@ -38,49 +38,55 @@
                     </div>
                 </div>
                 <div class="row mt-3">
-                    <div class="col-md border border-light rounded p-3 text-light">
-                        <h5> E-mail Profissional</h5>
-
-                        <p class=" text-center m-3">
-                            <strong class="letramaisclara"> "Credibilidade para o seu endereço eletrônico".</strong> <br>
-                            Eviar e-mail com o nome do seu negócio, transmite o comprometimento da empresa com a comunicação com o cliente.
-
-                        </p>
+                    <div class="col-md">
+                        <div class="row border border-light rounded p-3 text-light">
+                            <h5 class="mb-4">Cartão de Visita Virtual</h5>
+                            <p class="text-center">Fundamental para o desenvolvimento do negócio o Cartão Virtual é a ferramenta que mantém contato com
+                                o cliente. <br> <Strong class="letramaisclara">É a sua autoapresentação!</Strong> <br> Reforça a <strong class="letramaisclara">credibilidade</strong> e a
+                                <strong class="letramaisclara">legitimidade</strong> do seu negócio.
+                            </p>
+                            <p class="list-group" id="list-tab" role="tablist "> <strong class="list-group-item list-group-item-action list-group-item-danger letramaisescura">Vantagens do Cartão Virtual</strong> </p>
+                            <li class="mt-2 letramaisclara"> Podem ir onde quer que vá!</li>
+                            <li class="mt-2 letramaisclara"> Facilidade de Acesso</li>
+                            <li class="mt-2 letramaisclara"> Indicação com apenas um click</li>
+                            <li class="mt-2 letramaisclara"> Divulgação em cascata</li>
+                            <li class="mt-2 letramaisclara"> Nunca acaba</li>
+                            <li class="mt-2 letramaisclara"> Facilidade para Atualizações</li>
+                            <li class="mt-2 letramaisclara"> Menor custo</li>
+                            <br>
+                            <br>
+            
+            
+            
+                        </div>
                     </div>
                     <div class="col-1"></div>
-                    <div class="col-md border border-light rounded p-3 text-light">
-                        <h5>Criação de Logomarcas</h5>
-                        <p class="m-3 text-center">Como etapa para a <strong class="letramaisclara">construção visual</strong> da sua empresa, ter uma logo profissional é essencial para a
-                            consolidação do seu negócio. <br>
+                    <div class="col-md ">
+                        <div class="row border border-light rounded p-3 text-light ml-3">
+                            <h5> E-mail Profissional</h5>
+        
+                                <p class=" text-center m-3">
+                                    <strong class="letramaisclara"> "Credibilidade para o seu endereço eletrônico".</strong> <br>
+                                    Eviar e-mail com o nome do seu negócio, transmite o comprometimento da empresa com a comunicação com o cliente.
+        
+                                </p>
 
-                        </p>
+                        </div>
+                        <div class="row border border-light rounded p-3 text-light ml-3 mt-5">
+                            <h5> Cardápio Virtual</h5>
+        
+                                <p class=" text-center m-3">
+                                    <strong class="letramaisclara"> descrever serviço
+        
+                                </p>
+
+                        </div>
                     </div>
+             
                 </div>
             </div>
-            <div class="col-1   "></div> <br>
-            <div class="col-md">
-                <div class="row border border-light rounded p-3 text-light">
-                    <h5 class="mb-4">Cartão de Visita Virtual</h5>
-                    <p class="text-center">Fundamental para o desenvolvimento do negócio o Cartão Virtual é a ferramenta que mantém contato com
-                        o cliente. <br> <Strong class="letramaisclara">É a sua autoapresentação!</Strong> <br> Reforça a <strong class="letramaisclara">credibilidade</strong> e a
-                        <strong class="letramaisclara">legitimidade</strong> do seu negócio.
-                    </p>
-                    <p class="list-group" id="list-tab" role="tablist "> <strong class="list-group-item list-group-item-action list-group-item-danger letramaisescura">Vantagens do Cartão Virtual</strong> </p>
-                    <li class="mt-2 letramaisclara"> Podem ir onde quer que vá!</li>
-                    <li class="mt-2 letramaisclara"> Facilidade de Acesso</li>
-                    <li class="mt-2 letramaisclara"> Indicação com apenas um click</li>
-                    <li class="mt-2 letramaisclara"> Divulgação em cascata</li>
-                    <li class="mt-2 letramaisclara"> Nunca acaba</li>
-                    <li class="mt-2 letramaisclara"> Facilidade para Atualizações</li>
-                    <li class="mt-2 letramaisclara"> Menor custo</li>
-                    <br>
-                    <br>
-
-
-
-                </div>
-            </div>
-
+        
+            
         </div>
         <!-- Fim Linha Serviços  -->
     </div>
