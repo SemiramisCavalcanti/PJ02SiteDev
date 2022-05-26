8f8a5ea71b886badb40b5084cd4c0abe382126b7
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-05-26 01:00:09 -0300

    colocando menuprincipal

--- paginas/fenixEcho.php
+++ paginas/fenixEcho.php
@@ -2,9 +2,19 @@
 <div class=" my-2 w-100 rounded menuprincipal">
     <div class="container container-fluid">
         <nav class="navbar logomarca">
-                <a class="navbar-expand-lg p-1" href="#home">
-                    <img src="img/FenixEchoBranca.png" alt="" >
-                </a>  
+            <a class="navbar-expand-lg p-1" href="#home">
+                <img src="img/FenixEchoBranca.png" alt="">
+            </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active text-ligth" aria-current="page" href="#servicos">Serviços</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#servicos">Link</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
 
         <!-- Corpo Site -->
