5aee1e6c3284405080017b4a6a277d318e11119e
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-05-26 15:18:57 -0300

    ajuste logomarca

--- paginas/fenixEcho.php
+++ paginas/fenixEcho.php
@@ -2,14 +2,27 @@
 <div class=" my-2 w-100 rounded menuprincipal">
     <div class="container container-fluid">
 
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container-fluid">
-                <a class="navbar-brand mt-3" href="#">Navbar</a>
+        <nav class="navbar navbar-expand-lg navbar-light row mt-0">
+            <div class="container-fluid col-sm">
+                <a class="navbar-brand" id="#home"><img src="img/FenixEchoBranca.png" alt="" width="120" ></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <div class="col"></div>
+                <div class="collapse navbar-collapse col-md" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-5 fs-4 mr-3">
+                        <li class="nav-item mt-3">
+                            <a class="nav-link active" aria-current="page" href="#servicos">Serviços</a>
+                        </li>
+                        <li class="nav-item mt-3 ml-3">
+                            <a class="nav-link active" aria-current="page" href="#sobre">Sobre</a>
+                        </li>
+                        <li class="nav-item mt-3">
+                            <a class="nav-link active" aria-current="page" href="#contato">Contato</a>
+                        </li>
+                        
+                    </ul>
+                    <ul class="navbar-nav me-auto mb-5 fs-4">
                         <li class="nav-item mt-3">
                             <a class="nav-link active" aria-current="page" href="#servicos">Serviços</a>
                         </li>
@@ -20,12 +33,8 @@
                             <a class="nav-link active" aria-current="page" href="#contato">Contato</a>
                         </li>
                         
-                     
                     </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                
                 </div>
             </div>
         </nav>
