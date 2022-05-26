5126ce73ee28e6a134ea38b2fd8b916ea91dd3b1
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-05-26 00:52:03 -0300

    posição logomarca

--- paginas/fenixEcho.php
+++ paginas/fenixEcho.php
@@ -1,67 +1,12 @@
 <!-- Menu Principal-->
-<div class="menuprincipal my-2 w-100 rounded">
+<div class=" my-2 w-100 rounded menuprincipal">
     <div class="container container-fluid">
-        <div class="row-sm menuprincipal mb-5" id="home">
-
-            <!-- Coluna Logomarca -->
-            <div class="col logomarca text-center">
-                <img src="img/FenixEchoBranca.png" alt="" class="mt-1 ml-0 pl-0 w-25">
-            </div>
-            <!-- Fim Coluna Logomarca -->
-
-            <!-- Coluna Partes/Paginas -->
-            <div class="col">
-                <div class="row">
-
-                    <div class="colletrasmenu mt-4">
-                        <h5>
-                            <a class="text-decoration-none letramaisescura" href="#servicos">
-                                Serviços
-                            </a>
-                        </h5>
-                    </div>
-                    <!-- <div class="col letrasmenu mt-5">
-                        <h5>
-                            <a href="#portifolio">
-                                Portifólio
-                            </a>
-                        </h5>
-                    </div> -->
-                    <div class="col letrasmenu mt-4">
-                        <h5>
-                            <a class="text-decoration-none letramaisescura" href="#sobre">
-                                Sobre
-                            </a>
-                        </h5>
-
-                    </div>
-                    <div class="col letrasmenu mt-4">
-                        <h5>
-                            <a class="text-decoration-none letramaisescura" href="#contato">
-                                Contato
-                            </a>
-                        </h5>
-                    </div>
-                </div>
-            </div>
-            <!-- Fim Coluna Partes/Paginas -->
-
-            <!-- Coluna Rede Sociais -->
-            <div class="col-md redessociais container-fluid ">
-                <a href="https://www.instagram.com/fenix_echo_web/" class="link-light text-decoration-none"> <i class="fab fa-instagram"></i> </a>
-                <a href="https://github.com/SemiramisCavalcanti" class="link-light text-decoration-none"> <i class="fab fa-github"></i> </a>
-                <a href="https://www.linkedin.com/in/semiramis-cavalcanti-01a18013b/" class="link-light text-decoration-none"> <i class="fab fa-linkedin"></i> </a>
-
-
-            </div>
-
-            <!-- Fim Coluna Redes Sociais -->
-
-            <div>
-            </div>
-        </div>
-        <!-- Fim Menu Principal -->
+        <nav class="navbar logomarca">
+                <a class="navbar-expand-lg p-1" href="#home">
+                    <img src="img/FenixEchoBranca.png" alt="" >
+                </a>  
+        </nav>
 
         <!-- Corpo Site -->
-    
+
         <img src="img/capa.png" alt="" class="my-3 w-100 img-fluid">
