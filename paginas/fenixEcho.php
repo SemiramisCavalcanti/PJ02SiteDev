b1b3d8146c4c301e6aebc57803bb8b1920f77676
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-05-26 15:50:22 -0300

    fim atualização menu principal

--- paginas/fenixEcho.php
+++ paginas/fenixEcho.php
@@ -3,33 +3,31 @@
     <div class="container container-fluid">
 
         <nav class="navbar navbar-expand-lg navbar-light row mt-0">
-            <div class="container-fluid col-sm">
+            <div class="container-fluid col-sm ms-5">
                 <a class="navbar-brand mx-5" id="home"><img src="img/FenixEchoBranca.png" alt="" width="120"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="col"></div>
+
                 <div class="collapse navbar-collapse col-md" id="navbarSupportedContent">
                     <ul class="navbar-nav text-center mb-5 fs-3">
-                        <li class="nav-item mt-3 me-5">
+                        <li class="nav-item mt-3 ms-5">
                             <a class="nav-link active" aria-current="page" href="#servicos">Serviços</a>
                         </li>
-                        <li class="nav-item mt-3 me-5">
+                        <li class="nav-item mt-3 ms-5">
                             <a class="nav-link active" aria-current="page" href="#sobre">Sobre</a>
                         </li>
-                        <li class="nav-item mt-3">
+                        <li class="nav-item mt-3 ms-5 me-5">
                             <a class="nav-link active" aria-current="page" href="#contato">Contato</a>
                         </li>
 
-                    </ul>
-                    <ul class="navbar-nav ms-auto mb-5 fs-4">
-                        <li class="nav-item mt-3 me-5">
+                        <li class="nav-item mt-3 ms-5">
                             <a href="https://www.instagram.com/fenix_echo_web/" class="link-light text-decoration-none"> <i class="fab fa-instagram"></i> </a>
                         </li>
-                        <li class="nav-item mt-3 me-4">
+                        <li class="nav-item mt-3 ms-4">
                             <a href="https://github.com/SemiramisCavalcanti" class="link-light text-decoration-none"> <i class="fab fa-github"> </i> </a>
                         </li>
-                        <li class="nav-item mt-3">
+                        <li class="nav-item mt-3 ms-4">
                             <a href="https://www.linkedin.com/in/semiramis-cavalcanti-01a18013b/" class="link-light text-decoration-none"> <i class="fab fa-linkedin"></i> </a>
                         </li>
 
