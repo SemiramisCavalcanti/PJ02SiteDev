3381dc9b192e966131e3b7b0efcee84cb7062242
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-05-27 13:40:03 -0300

    orçamento via whatsapp - sem formulario php (revisar)

--- paginas/topicos/contato-localizacao.php
+++ paginas/topicos/contato-localizacao.php
@@ -1,40 +1,40 @@
 <div class="container" id="contato">
 
-<div class="row">
-    <div class="col-md text-light">
-        <h4 class="mt-5 ml-3">
-            Contato
-            <hr>
-        </h4>
-       
-    </div>
-    <div class="col-md p-5 mt-5">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d987.9636949564411!2d-34.836090670849856!3d-7.910235071299581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab3e41689ca26d%3A0x1f6fa8664d23562a!2sAv.%20Costa%20Azul%2C%201208-1066%20-%20Pau%20Amarelo%2C%20Paulista%20-%20PE%2C%2053433-240!5e0!3m2!1spt-BR!2sbr!4v1643112718567!5m2!1spt-BR!2sbr" width="500" class="border border-5 rounded" height="300" allowfullscreen="" loading="lazy"></iframe>
+    <div class="row">
+        <div class="col-md text-light">
+            <h4 class="mt-5 ml-3">
+                Contato
+                <hr>
+            </h4>
+            <div class="text-light text-center mt-3">
+                <!-- Sobre orçamento e localiade -->
+                <h4>Somos uma Empresa 100% Virtual, Desenvoldores de WebSites e Ferramentas Digitais. <br> <br> "Entregamos nossos serviços onde estiver."</h4>
+                <p>Salvo exceções dispomos de Reunião Presencial (verificar disponibilidade de horários e custos)
+                </p>
+                <br>
+                <a href="https://wa.me/message/AQSI3RMOO6DVL1" class="decoration-none"> <button class="btn btn-outline-light"><i class="fab fa-whatsapp"></i> Faça já seu orçamento.</button></a>
+            </div>
+        </div>
+        <div class="col-md p-5 mt-5">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d987.9636949564411!2d-34.836090670849856!3d-7.910235071299581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab3e41689ca26d%3A0x1f6fa8664d23562a!2sAv.%20Costa%20Azul%2C%201208-1066%20-%20Pau%20Amarelo%2C%20Paulista%20-%20PE%2C%2053433-240!5e0!3m2!1spt-BR!2sbr!4v1643112718567!5m2!1spt-BR!2sbr" width="500" class="border border-5 rounded" height="300" allowfullscreen="" loading="lazy"></iframe>
+
+        </div>
 
     </div>
 
-</div>
-<div class="text-light text-center mt-3">
-    <!-- Sobre orçamento e localiade -->
-    <h4>Somos uma Empresa 100% Virtual, Desenvoldores de WebSites e Ferramentas Digitais. <br> "Entregamos nossos serviços onde estiver."</h4>
-    <p>Salvo exceções dispomos de Reunião Presencial (verificar disponibilidade de horários e custos)
-    </p>
-    <br>
-    <a href="#contato" class="decoration-none"> <button class="btn btn-outline-light">Faça já seu orçamento.</button></a>
-</div>
-<!-- Linha Contato  -->
+    <!-- Linha Contato  -->
 
-<!-- Linha Reuniões On-line/Localização -->
-<div class="row">
-    <!-- Coluna Reuniões On-line -->
-    <div class="col">
+    <!-- Linha Reuniões On-line/Localização -->
+    <div class="row">
+        <!-- Coluna Reuniões On-line -->
+        <div class="col">
+        </div>
+        <!-- Fim Coluna Reuniões On-line -->
+        <!-- Coluna Localização -->
+        <div class="col">
+        </div>
+        <!-- Fim Coluna Localização -->
     </div>
-    <!-- Fim Coluna Reuniões On-line -->
-    <!-- Coluna Localização -->
-    <div class="col">
-    </div>
-    <!-- Fim Coluna Localização -->
-</div>
-<!-- Fim Linha Reuniões On-line/Localização -->
+    <!-- Fim Linha Reuniões On-line/Localização -->
 
 </div>
