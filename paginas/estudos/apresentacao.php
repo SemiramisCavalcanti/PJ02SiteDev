d3a1a362f5ffc378839d8b2b4360d9d0e55894c0
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-08-01 10:43:36 -0300

    juntando dois sites do mesma pessoa,

---
+++ paginas/estudos/apresentacao.php
@@ -0,0 +1,8 @@
+<div id="itens-wrapper"> 
+    <div id="itens">
+        <img src="img\1.png" alt="" class="item">
+    </div>
+    <div id="itens">
+        <img src="img\2.png" alt="" class="item">
+    </div>
+</div>
---
+++ paginas/institucional/sobre.php
@@ -0,0 +1,74 @@
+   <!-- Linha Sobre-->
+   <div class="container" id="sobre">
+
+       <div class="row">
+           <!-- Coluna 01 -->
+           <div class="col-md text-light mt-5">
+               <!-- A empresa -->
+               <h4>
+                   A Empresa
+                   <hr>
+               </h4>
+               <p class="text-center">Atuamos no mercado de tecnologia, na construção virtual da sua empresa, priorizando a qualidade para
+                   melhor alcançar os objetivos de nossos clientes, oferecemos serviços como: desenvolvimento de sites,
+                   e-mail profissional, cartão de visitas virtual e cardápios virtual.
+               </p>
+               <div class="row">
+                   <!-- <div class="col-sm">
+                       <img src="" alt="">
+                   </div> -->
+                   <!-- Quem Sou -->
+                   <div class="col-sm">
+                       <h5 class="mt-3">
+                           Quem Sou?
+                       </h5>
+                       <p>Desenvolvedora Web HTML | CSS | JS | PHP
+                           <br>
+                           Universitária em Análise e Desenvolvimento de Sistemas
+                       </p>
+                       <p></p>
+                   </div>
+
+               </div>
+           </div>
+
+           <!-- Coluna 02 -->
+           <div class="col-sm text-light mt-5 ">
+               <div class="card m-auto mt-5 ">
+                   <div class="card-header  mt-3 titulocard elevation-4">
+
+                   </div>
+                   <div class="card-body mt-sm">
+                       <div class="text-center letramaisescura">
+                           <strong>Ter um ambiente virtual não é mais um diferencial, <br> é um requisito do mercado. </strong>
+                           <br>
+                           Na Era Digital onde nossas referências, produtos/serviços, parceiros, projetos e feedbacks de clientes estão nas pontas dos dedos,
+                           temos a facilidade de ofertar, vender e entregar nossos produtos tudo de forma on-line.
+                           <br>
+                           <strong>Então um ambiente Virtual se tornou essencial e nós ajudamos você, a sua marca e a sua empresa a se conectar com o mundo e suas oportunidades.</strong>
+                           <hr>
+                       </div>
+
+                   </div>
+
+                   <!-- 
+    texto para colocar
+               <div>
+                       E quais seus ganhos reais?
+                   </div>
+                   <div class="text-center">
+                       <strong> - Primeiro, aumenta seu alcance!</strong> Seus produtos e/ou serviços conhecidos nos mais diversos lugares! <br>
+                       <strong> Ah! E transmite confiança e credibilidade!</strong> - Quantas vezes você procurou os comentários na internet sobre determinado local? <br>
+                       E se comentam é porque vale a pena comprar naquele local. <strong>Mantenha seus clientes satisfeitos!
+                           Isto só Fortalece sua marca.</strong> <br> Também <strong>Cria autoridade,</strong> faz de você respeitado, referência.
+                       E o melhor que tudo isso reflete em <strong>suas vendas, aumentando-as</strong>.
+                       <br>
+                       Venha fazer negócio! Peça já seu orçamento!
+                       <hr>
+                   </div> -->
+               </div>
+               <!-- Fim Coluna Contato -->
+           </div>
+           <!-- Fim Linha Sobre -->
+       </div>
+   </div>
